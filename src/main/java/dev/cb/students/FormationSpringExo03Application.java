9986838f9868c537da437baf66a70dc91fe1d4c4
cedric <cedric.belot.dev@gmail.com>
2024-07-22 11:02:50 +0200

    initial commit

---
+++ src/main/java/dev/cb/students/FormationSpringExo03Application.java
@@ -0,0 +1,13 @@
+package dev.cb.students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationSpringExo03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationSpringExo03Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cb/students/FormationSpringExo03ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cb.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationSpringExo03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
